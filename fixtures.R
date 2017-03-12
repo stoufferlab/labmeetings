96ee12fee6d3cc062c66a05f4140b44676b12fef
Fernando Cagua <efcaguab@gmail.com>
2017-03-13 10:57:39 +1300

    Add opt parser

--- fixtures.R
+++ fixtures.R
@@ -1,6 +1,18 @@
+#!/usr/bin/env Rscript
 library(magrittr)
+library(optparse)
 
-m <- read.csv("doodle_results_Feb2017.csv", row.names = 1)
+option_list <- list(
+	make_option(c("-i", "--input"), type = "character", default = NULL, 
+							help = "input csv file, must contain people names in first column & dates in first row. 0 for not available, 0.5 for available but not preferred, and 1 for preferred"),
+	make_option(c("-s", "--seed"), type = "integer", default = 1,
+							help = "random seed for computation")
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+m <- read.csv(opt$input, row.names = 1)
 m <- m[order(rowSums(m)), ]
 m <- m[, order(colSums(m))]
 
@@ -20,7 +32,6 @@ choose_per <- function(x, fixture, seed = 1) {
 	}
 }
 
-set.seed(2017)
 fixture <- data.frame(person = NA, date = NA)
 choose_per(m, fixture) %>% 
 	# dplyr::mutate(date  = as.POSIXct(date, format = "X%Y.%m.%d")) %>%
