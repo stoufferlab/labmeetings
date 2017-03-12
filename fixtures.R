3a96bf1b84f8b3c4ccaa38081b4e4e9d1ca2bbce
Fernando Cagua <efcaguab@gmail.com>
2017-03-13 10:58:09 +1300

    accomodate  for when n_people != n_meetings * 2

--- fixtures.R
+++ fixtures.R
@@ -13,27 +13,43 @@ opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
 m <- read.csv(opt$input, row.names = 1)
+if(ncol(m) * 2 < nrow(m)) warning("Not enough meeting dates to accomodate all the people")
+if(ncol(m) * 2 > nrow(m)) warning("Not enough people to fill all the meetings")
+
+modulus <- nrow(m) %% ncol(m)
+last_date <- NULL
+if(modulus != 0) last_date <- colnames(m)[ncol(m)]
+if(!is.null(last_date)) warning("Only one person in last meeting")
+
 m <- m[order(rowSums(m)), ]
 m <- m[, order(colSums(m))]
 
-choose_per <- function(x, fixture, seed = 1) {
-	# print(x)
+choose_per <- function(x, fixture, seed = 1, last_date = NULL) {
 	set.seed(seed)
-	chosen <- sample(rownames(x), 2, prob = x[, 1])
+	if(!is.null(last_date)){
+		if(colnames(x)[1] == last_date) {
+			n <- 1
+		} else {
+			n <- 2
+		}
+	} else {
+		n <- 2
+	}
+	chosen <- sample(rownames(x), n, prob = x[, 1])
 	fixture %<>% dplyr::bind_rows(
 		dplyr::data_frame(person = chosen, 
 											date = colnames(x)[1]))
 	if(all(dim(x) > 1)){
 		x <- x[!rownames(x) %in% chosen, , drop = F]
 		x <- x[, -1, drop = F]
-		choose_per(x, fixture, seed + 1)
+		choose_per(x, fixture, seed + 1, last_date)
 	} else {
 		return(fixture)
 	}
 }
 
 fixture <- data.frame(person = NA, date = NA)
-choose_per(m, fixture) %>% 
+choose_per(m, fixture, seed = opt$seed, last_date) %>% 
 	# dplyr::mutate(date  = as.POSIXct(date, format = "X%Y.%m.%d")) %>%
 	dplyr::filter(!is.na(date)) %>%
 	dplyr::arrange(date)
