62e3d1a10ac41cb36e2dcae091dd5c767afd76ac
Fernando Cagua <efcaguab@gmail.com>
2017-04-07 18:59:06 +1200

    Save results to a csv and add parse dates

--- fixtures.R
+++ fixtures.R
@@ -5,8 +5,12 @@ library(optparse)
 option_list <- list(
 	make_option(c("-i", "--input"), type = "character", default = NULL, 
 							help = "input csv file, must contain people names in first column & dates in first row. 0 for not available, 0.5 for available but not preferred, and 1 for preferred"),
+	make_option(c("-o", "--output"), type = "character", default = NULL,
+							help = "output csv with the fixtures"),
 	make_option(c("-s", "--seed"), type = "integer", default = 1,
-							help = "random seed for computation")
+							help = "random seed for computation"),
+	make_option(c("-f", "--date_format"), type = "character", default = NULL,
+							help = "format of the dates in the csv header")
 )
 
 opt_parser <- OptionParser(option_list = option_list)
@@ -49,7 +53,22 @@ choose_per <- function(x, fixture, seed = 1, last_date = NULL) {
 }
 
 fixture <- data.frame(person = NA, date = NA)
-choose_per(m, fixture, seed = opt$seed, last_date) %>% 
+fixture <- choose_per(m, fixture, seed = opt$seed, last_date) %>% 
 	# dplyr::mutate(date  = as.POSIXct(date, format = "X%Y.%m.%d")) %>%
 	dplyr::filter(!is.na(date)) %>%
-	dplyr::arrange(date)
+	dplyr::mutate(date = gsub("\\.", " ", date))
+
+if(!grepl("[y,Y,x,X]", opt$date_format)){
+	fixture %<>%
+		dplyr::mutate(date = paste(lubridate::year(Sys.Date()), date))
+	opt$date_format <- paste("%Y", opt$date_format)
+}
+
+if(is.null(opt$output)) opt$output <- paste0(opt$input, "_out.csv")
+
+fixture %>%
+	dplyr::mutate(date = as.Date(date, format = opt$date_format)) %>%
+	dplyr::arrange(date) %>%
+	dplyr::group_by(date) %>%
+	dplyr::summarise(person = toString(person)) %>%
+	readr::write_csv(opt$output)
